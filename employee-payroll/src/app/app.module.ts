4566c496682e412856e5cef9e8a28a39394ca425
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-09 10:55:39 +0530

    Added folder structure and also created components

--- employee-payroll/src/app/app.module.ts
+++ employee-payroll/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PayrollFormComponent } from './components/payroll-form/payroll-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PayrollFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ employee-payroll/src/app/components/payroll-form/payroll-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payroll-form',
+  templateUrl: './payroll-form.component.html',
+  styleUrls: ['./payroll-form.component.scss']
+})
+export class PayrollFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
