e903fbf68802d4d4cbb94fe30dfb951f82ddaeed
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-10 16:29:00 +0530

    Completed with display and passed data from parent to child component

--- employee-payroll/src/app/components/home/home.component.ts
+++ employee-payroll/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from "../../services/employee-service/employee.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private emplyeeService: EmployeeService) { }
+  employeeArray: Array<any> = [];
 
   ngOnInit() {
+    this.getAllEmployee();
+  }
+
+  getAllEmployee(): void {
+    this.emplyeeService.getAllEmployee().subscribe(data => {
+      this.employeeArray = <any>data;
+      console.log("data after getAllEmployee", this.employeeArray);
+
+    }, err => {
+      console.log("err after getAllEmployee", err);
+    })
   }
 
 }
