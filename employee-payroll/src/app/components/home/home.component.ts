808055a266942c464bd9f2af6cb3f69d7e06835a
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-11 19:32:34 +0530

    Completed with display update delete add

--- employee-payroll/src/app/components/home/home.component.ts
+++ employee-payroll/src/app/components/home/home.component.ts
@@ -13,16 +13,26 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.getAllEmployee();
+    // clear data of update employee from local storage
+    localStorage.removeItem('editEmp')
   }
 
   getAllEmployee(): void {
     this.emplyeeService.getAllEmployee().subscribe(data => {
       this.employeeArray = <any>data;
-      console.log("data after getAllEmployee", this.employeeArray);
+      // console.log("data after getAllEmployee", this.employeeArray);
 
     }, err => {
       console.log("err after getAllEmployee", err);
     })
+  }
+  /**
+   * description:-this method is for catching event from child component 
+   * @param data 
+   */
+  updateEvent(data) {
+    if (data.type == 'delete')
+      this.getAllEmployee()
   }
 
 }
