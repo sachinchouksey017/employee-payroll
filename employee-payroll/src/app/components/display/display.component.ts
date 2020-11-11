42f3dcb7b2b85af957747c9555bb225c4eb708b2
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-11 19:31:22 +0530

    Display data is completed

--- employee-payroll/src/app/components/display/display.component.ts
+++ employee-payroll/src/app/components/display/display.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeeService } from '../../services/employee-service/employee.service';
+
+interface employeeType {
+  type: string,
+  data: {}
+}
 
 @Component({
   selector: 'app-display',
@@ -7,12 +14,23 @@ import { Component, Input, OnInit, Output } from '@angular/core';
 })
 export class DisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService, private router: Router) { }
+  // input decorator to share data from parent to child
   @Input() employeeArray = [];
-
+  // event emiter to share data from child to parent
+  @Output() employeeEvent = new EventEmitter<employeeType>();
   ngOnInit() {
   }
   remove(employeeId: string) {
+    this.employeeService.deleteEmployee(employeeId).subscribe(data => {
+      this.employeeEvent.emit({ type: 'delete', data: {} })
+    }, err => {
+
+    })
+  }
+  update(element) {
+    localStorage.setItem('editEmp', JSON.stringify(element));
+    this.router.navigateByUrl('payroll-form');
   }
 
 }
