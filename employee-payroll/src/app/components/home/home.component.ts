6282832b5a331600d47516f209684b8be2d113e5
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-21 17:27:07 +0530

    Added: search functionality with responsive

--- employee-payroll/src/app/components/home/home.component.ts
+++ employee-payroll/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { EmployeeService } from "../../services/employee-service/employee.service";
 
 @Component({
@@ -10,6 +11,9 @@ export class HomeComponent implements OnInit {
 
   constructor(private emplyeeService: EmployeeService) { }
   employeeArray: Array<any> = [];
+  employeeArrayAll: Array<any> = [];
+  searchExpand: boolean = false;
+  searchKey = new FormControl('');
 
   ngOnInit() {
     this.getAllEmployee();
@@ -20,6 +24,9 @@ export class HomeComponent implements OnInit {
   getAllEmployee(): void {
     this.emplyeeService.getAllEmployee().subscribe(data => {
       this.employeeArray = <any>data;
+      this.employeeArrayAll = <any>data;
+      if (this.searchKey.value.trim().length > 0)
+        this.search()
       // console.log("data after getAllEmployee", this.employeeArray);
 
     }, err => {
@@ -33,6 +40,16 @@ export class HomeComponent implements OnInit {
   updateEvent(data) {
     if (data.type == 'delete')
       this.getAllEmployee()
+  }
+  search(): void {
+    // assigning the original array to employeeArray
+    this.employeeArray = this.employeeArrayAll;
+    // after filter reassign the filter array to employee array
+    if (this.searchKey.value.trim().length > 0)
+      this.employeeArray = this.employeeArray.filter(element => element.name.toLowerCase().indexOf(this.searchKey.value.toLowerCase()) > -1);
+  }
+  openSearchBox(): void {
+    this.searchExpand = true;
   }
 
 }
