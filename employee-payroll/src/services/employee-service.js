08923768f7eb3755435898e41740ca9539ae7669
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-23 01:37:53 +0530

    Added employee and axios services

---
+++ employee-payroll/src/services/employee-service.js
@@ -0,0 +1,20 @@
+import config from '../config/config';
+import AxiosService from './axois-service'
+export default class EmployeeService {
+    baseUrl = config.baseUrl;
+    getAllEmployee() {
+        return AxiosService.getService(`${this.baseUrl}employee`);
+    }
+    getEmployee(id) {
+        return AxiosService.getService(`${this.baseUrl}employee/${id}`);
+    }
+    deleteEmployee(id) {
+        return AxiosService.deleteService(`${this.baseUrl}employee/${id}`)
+    }
+    addEmployee(data) {
+        return AxiosService.postService(`${this.baseUrl}employee`, data);
+    }
+    updateEmployee(data) {
+        return AxiosService.putService(`${this.baseUrl}employee/${data.id}`, data)
+    }
+}
