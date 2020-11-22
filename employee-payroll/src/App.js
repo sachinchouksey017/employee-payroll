b35c94fca31350075d38285118f0c9230ce8c777
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-23 01:39:24 +0530

    Display is completed

--- employee-payroll/src/App.js
+++ employee-payroll/src/App.js
@@ -1,10 +1,13 @@
 import Toolbar from './components/header/header.jsx'
+import Home from './components/home/home.jsx';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Toolbar />
+      <Home />
       hello
     </div>
   );
