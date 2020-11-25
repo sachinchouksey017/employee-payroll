afab19efbd88bdfa729ede47d4e356b7452c2a4a
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-25 22:56:47 +0530

    Refactor code to fix profile problem

--- employee-payroll/src/components/display/display.jsx
+++ employee-payroll/src/components/display/display.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import './display.scss'
 import deleteIcon from '../../assets/icons/delete-black-18dp.svg'
 import editIcon from '../../assets/icons/create-black-18dp.svg'
-import profile from '../../assets/profile-images/Ellipse -1.png'
+import profile1 from '../../assets/profile-images/Ellipse -3.png';
+import profile2 from '../../assets/profile-images/Ellipse 1.png';
+import profile3 from '../../assets/profile-images/Ellipse -8.png';
+import profile4 from '../../assets/profile-images/Ellipse -7.png';
 import { withRouter } from "react-router-dom";
 import EmployeeService from '../../services/employee-service'
 
@@ -26,6 +29,10 @@ const Display = (props) => {
         // this.router.navigateByUrl(`payroll-form/${employeeId}`);
         props.history.push(`payroll-form/${employeeId}`)
     }
+    const getProfile = (url) => {
+        console.log("url is", url);
+        return profile1;
+    }
 
     return (
         <table id="display" className="display">
@@ -42,7 +49,10 @@ const Display = (props) => {
                 {
                     props.employeeArray && props.employeeArray.map((element, ind) => (
                         <tr key={ind}>
-                            <td><img className="profile" src={profile} alt="" /></td>
+                            <td><img className="profile" src={element.profileUrl === '../../assets/profile-images/Ellipse -3.png' ?
+                                profile1 : element.profileUrl === '../../assets/profile-images/Ellipse 1.png' ?
+                                    profile2 : element.profileUrl === '../../assets/profile-images/Ellipse -8.png' ?
+                                        profile3 : profile4} alt="" /></td>
                             <td>{element.name}</td>
                             <td>{element.gender}</td>
                             <td>
